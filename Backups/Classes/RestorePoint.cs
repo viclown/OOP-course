d4c1dea1553ddf6c689fb6fc9a2c625b06ae248c
viclown <76227700+viclown@users.noreply.github.com>
2021-11-20 20:38:31 +0300

    Create RestorePoint.cs

---
+++ Backups/Classes/RestorePoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace Backups.Classes
+{
+    public class RestorePoint
+    {
+        public RestorePoint(DirectoryInfo restorePointDirectory, List<ZipArchive> zipArchives, DateTime date, Algorithm algorithm)
+        {
+            RestorePointDirectory = restorePointDirectory;
+            ZipArchives = zipArchives;
+            Date = date;
+            Algorithm = algorithm;
+        }
+
+        public RestorePoint(DirectoryInfo restorePointDirectory, DateTime date, Algorithm algorithm)
+            : this(restorePointDirectory, new List<ZipArchive>(), date, algorithm) { }
+
+        public DirectoryInfo RestorePointDirectory { get; }
+        public List<ZipArchive> ZipArchives { get; set; }
+        public DateTime Date { get; }
+        public Algorithm Algorithm { get; }
+    }
+}
