0d36d430d3658b857b6daced5557d05e0dd445bf
viclown <76227700+viclown@users.noreply.github.com>
2021-11-20 20:37:39 +0300

    Create BackupService.cs

---
+++ Backups/Services/BackupService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Backups.Classes;
+
+namespace Backups.Services
+{
+    public class BackupService
+    {
+        private List<BackupJob> BackupJobs { get; } = new List<BackupJob>();
+
+        public BackupJob CreateNewBackupJob(string path, string directoryBackupJobName)
+        {
+            DirectoryInfo directoryBackupJob = Directory.CreateDirectory(path + @"\" + directoryBackupJobName);
+            Directory.CreateDirectory(directoryBackupJob.FullName + @"\JobObjects");
+            var backupJob = new BackupJob(directoryBackupJob);
+            BackupJobs.Add(backupJob);
+            return backupJob;
+        }
+    }
+}
