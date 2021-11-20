0f8ef7df0706d55be436b6a8dc9e355a084b0fc3
viclown <76227700+viclown@users.noreply.github.com>
2021-11-20 21:15:16 +0300

    Update BackupServiceTest.cs

--- Backups.Tests/BackupServiceTest.cs
+++ Backups.Tests/BackupServiceTest.cs
@@ -12,13 +12,6 @@ namespace Backups.Tests
         {
             var backupService = new BackupService();
             BackupJob backupJob = backupService.CreateNewBackupJob(@"C:\Users\Виктория\Desktop\OOP\viclown\Backups", "BackupJob");
-            var fileA = new FileInfo(@"C:\Users\Виктория\Desktop\OOP\viclown\Backups\FileA");
-            var fileB = new FileInfo(@"C:\Users\Виктория\Desktop\OOP\viclown\Backups\FileB");
-            backupJob.AddObjectToBackupJob(fileA);
-            backupJob.AddObjectToBackupJob(fileB);
-            RestorePoint restorePoint1 = backupJob.CreateRestorePoint(backupJob.BackupDirectory.FullName, Algorithm.Single);
-            backupJob.DeleteObjectFromBackupJob("fileB");
-            RestorePoint restorePoint2 = backupJob.CreateRestorePoint(backupJob.BackupDirectory.FullName, Algorithm.Single);
         }
     }
 }
