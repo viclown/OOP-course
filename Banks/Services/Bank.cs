9d3983e62e247eb81ed919109f1eb991a0e75353
viclown <76227700+viclown@users.noreply.github.com>
2021-12-28 22:31:43 +0300

    add bank and central bank

---
+++ Banks/Services/Bank.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Banks.Classes;
+using Banks.Classes.BankAccount;
+
+namespace Banks.Services
+{
+    public class Bank
+    {
+        private int _lastBankId = 0;
+
+        public Bank(string name, BankInterest interest, BankCommission commission, BankLimit limitForSuspiciousStudents, BankLimit limitForCreditAccount, DateTime currentDate)
+        {
+            Name = name;
+            Id = _lastBankId++;
+            Interest = interest;
+            Commission = commission;
+            LimitForSuspiciousClients = limitForSuspiciousStudents;
+            LimitForCreditAccount = limitForCreditAccount;
+            Clients = new List<Client>();
+            CurrentDate = currentDate;
+        }
+
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public BankInterest Interest { get; set; }
+        public BankCommission Commission { get; set; }
+        public BankLimit LimitForSuspiciousClients { get; set; }
+        public BankLimit LimitForCreditAccount { get; set; }
+        public List<Client> Clients { get; set; }
+        public List<DebitAccount> DebitAccounts { get; set; } = new List<DebitAccount>();
+        public List<DepositAccount> DepositAccounts { get; set; } = new List<DepositAccount>();
+        public List<CreditAccount> CreditAccounts { get; set; } = new List<CreditAccount>();
+        public DateTime CurrentDate { get; set; }
+
+        public Client AddNewClientToBank(Client client)
+        {
+            Clients.Add(client);
+            return client;
+        }
+
+        public void SendNotificationsToClients(string message, List<Client> clients)
+        {
+            foreach (Client client in clients.Where(client => client.IsSubscribedToNotifications))
+            {
+                client.Notifications.Add(message);
+            }
+        }
+
+        public BankCommission SetNewBankCommission(BankCommission commission)
+        {
+            Commission = commission;
+            string message = $"We have set new bank commissions for all our clients. New bank commission is {Commission.Value}";
+            SendNotificationsToClients(message, Clients);
+            return commission;
+        }
+
+        public BankLimit SetNewBankLimitForSuspiciousClients(BankLimit limitForSuspiciousClients)
+        {
+            LimitForSuspiciousClients = limitForSuspiciousClients;
+            string message = $"We have set new bank limits for clients without passport or address information. New bank limit for these clients is {LimitForSuspiciousClients.Value}";
+            SendNotificationsToClients(message, Clients);
+            return limitForSuspiciousClients;
+        }
+
+        public BankLimit SetNewBankLimitForCreditAccount(BankLimit limitForCreditAccount)
+        {
+            LimitForCreditAccount = limitForCreditAccount;
+            string message = $"We have set new bank limits for credit accounts. New bank limit for credit accounts is {LimitForCreditAccount.Value}";
+            SendNotificationsToClients(message, Clients);
+            return limitForCreditAccount;
+        }
+
+        public BankInterest SetNewBankInterest(BankInterest interest)
+        {
+            Interest = interest;
+            string message = $"We have set new bank interests on balances. New bank interest is {Interest.Value}";
+            SendNotificationsToClients(message, Clients);
+            return interest;
+        }
+
+        public void CheckNewDay()
+        {
+            CurrentDate = CurrentDate.AddDays(1);
+            foreach (Client client in Clients)
+            {
+                foreach (DebitAccount account in client.DebitAccounts)
+                {
+                    account.CheckNewDay();
+                }
+
+                foreach (CreditAccount account in client.CreditAccounts)
+                {
+                    account.CheckNewDay();
+                }
+
+                foreach (DepositAccount account in client.DepositAccounts)
+                {
+                    account.CheckNewDay();
+                }
+            }
+        }
+    }
+}
---
+++ Banks/Services/CentralBank.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Banks.Classes;
+using Banks.Classes.BankAccount;
+using Banks.Tools;
+
+namespace Banks.Services
+{
+    public class CentralBank
+    {
+        public CentralBank()
+        {
+            Banks = new List<Bank>();
+            CurrentDate = DateTime.Today;
+        }
+
+        public List<Bank> Banks { get; set; }
+        public DateTime CurrentDate { get; set; }
+
+        public Bank CreateNewBank(string name, double interest, double commission, double limitForSuspiciousClients, double limitForCreditAccount)
+        {
+            var bankInterest = new BankInterest(interest);
+            var bankCommission = new BankCommission(commission);
+            var bankLimitForSuspiciousClients = new BankLimit(limitForSuspiciousClients);
+            var bankLimitForCreditAccount = new BankLimit(limitForCreditAccount);
+            var bank = new Bank(name, bankInterest, bankCommission, bankLimitForSuspiciousClients, bankLimitForCreditAccount, CurrentDate);
+            Banks.Add(bank);
+            return bank;
+        }
+
+        public DebitAccount CreateDebitAccount(Client client, Bank bank)
+        {
+            var account = new DebitAccount(client, bank);
+            bank.DebitAccounts.Add(account);
+            client.DebitAccounts.Add(account);
+            return account;
+        }
+
+        public DepositAccount CreateDepositAccount(Client client, int durationInDays, Bank bank)
+        {
+            var account = new DepositAccount(client, bank, DateTime.Now.AddDays(durationInDays));
+            bank.DepositAccounts.Add(account);
+            client.DepositAccounts.Add(account);
+            return account;
+        }
+
+        public CreditAccount CreateCreditAccount(Client client, Bank bank)
+        {
+            var account = new CreditAccount(client, bank);
+            bank.CreditAccounts.Add(account);
+            client.CreditAccounts.Add(account);
+            return account;
+        }
+
+        public Bank FindBank(string name)
+        {
+            foreach (Bank bank in Banks)
+            {
+                if (bank.Name == name)
+                    return bank;
+            }
+
+            throw new BankWasNotFoundException($"Bank with a name {name} does not exist");
+        }
+
+        public void RunTimeMechanism(int durationInDays)
+        {
+            for (int day = 0; day < durationInDays; day++)
+            {
+                CurrentDate = CurrentDate.AddDays(1);
+                foreach (Bank bank in Banks)
+                   bank.CheckNewDay();
+            }
+        }
+    }
+}
