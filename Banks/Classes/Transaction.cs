98e892d5ec3b7407a770b71233ff990d44062652
viclown <76227700+viclown@users.noreply.github.com>
2021-12-28 22:30:08 +0300

    add transaction

---
+++ Banks/Classes/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Banks.Classes
+{
+    public class Transaction
+    {
+        public Transaction(Client receiver, Client sender, double amountOfTransaction, DateTime transactionTime)
+        {
+            Receiver = receiver;
+            Sender = sender;
+            AmountOfTransaction = amountOfTransaction;
+            TransactionTime = transactionTime;
+        }
+
+        public Client Receiver { get; }
+        public Client Sender { get; }
+        public double AmountOfTransaction { get; }
+        public DateTime TransactionTime { get; }
+        public bool WasDeclined { get; set; } = false;
+    }
+}
