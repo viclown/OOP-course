73795abdf951c0718d8c4e2b3364b55f2a138013
viclown <76227700+viclown@users.noreply.github.com>
2021-12-29 00:05:54 +0300

    commented local tests

--- Backups.Tests/BackupServiceTest.cs
+++ Backups.Tests/BackupServiceTest.cs
@@ -10,15 +10,16 @@ namespace Backups.Tests
         [Test]
         public void BackupJobTest1()
         {
-            var backupService = new BackupService();
-            BackupJob backupJob = backupService.CreateNewBackupJob(@"C:\Users\Виктория\Desktop\OOP\viclown\Backups", "BackupJob");
-            var fileA = new FileInfo(@"C:\Users\Виктория\Desktop\OOP\viclown\Backups\FileA");
-            var fileB = new FileInfo(@"C:\Users\Виктория\Desktop\OOP\viclown\Backups\FileB");
-            backupJob.AddObjectToBackupJob(fileA);
-            backupJob.AddObjectToBackupJob(fileB);
-            RestorePoint restorePoint1 = backupJob.CreateRestorePoint(backupJob.BackupDirectory.FullName, Algorithm.Single);
-            backupJob.DeleteObjectFromBackupJob("fileB");
-            RestorePoint restorePoint2 = backupJob.CreateRestorePoint(backupJob.BackupDirectory.FullName, Algorithm.Single);
+            // var backupService = new BackupService();
+            // IBackupService backup = new SplitAlgorithm();
+            // BackupJob backupJob = backupService.CreateNewBackupJob(@"C:\Users\Виктория\Desktop\OOP\viclown\Backups", "BackupJob", backup);
+            // var fileA = new FileInfo(@"C:\Users\Виктория\Desktop\OOP\viclown\Backups\FileA");
+            // var fileB = new FileInfo(@"C:\Users\Виктория\Desktop\OOP\viclown\Backups\FileB");
+            // backupJob.AddObjectToBackupJob(fileA);
+            // backupJob.AddObjectToBackupJob(fileB);
+            // RestorePoint restorePoint1 = backupJob.CreateRestorePoint(backupJob.BackupDirectory.FullName);
+            // backupJob.DeleteObjectFromBackupJob("fileB");
+            // RestorePoint restorePoint2 = backupJob.CreateRestorePoint(backupJob.BackupDirectory.FullName);
         }
     }
 }
