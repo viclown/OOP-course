318b9a2e10b510eb36d462cad8d2386de3215966
viclown <76227700+viclown@users.noreply.github.com>
2021-12-31 04:27:48 +0300

    fixed naming

---
+++ Backups/Classes/SingleSaver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Backups.Classes
+{
+    public class SingleSaver : IBackupSaver
+    {
+        public List<Storage> Save(DirectoryInfo backupDirectory)
+        {
+            var storage = new Storage();
+            FileInfo[] readFiles = backupDirectory.GetFiles();
+
+            foreach (FileInfo readFile in readFiles)
+            {
+                storage.AddFileToStorage(readFile);
+            }
+
+            var returnFiles = new List<Storage> { storage };
+            return returnFiles;
+        }
+    }
+}
---
+++ Backups/Classes/SplitSaver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Backups.Classes
+{
+    public class SplitSaver : IBackupSaver
+    {
+        public List<Storage> Save(DirectoryInfo backupDirectory)
+        {
+            var returnFiles = new List<Storage>();
+            FileInfo[] readFiles = backupDirectory.GetFiles();
+
+            foreach (FileInfo readFile in readFiles)
+            {
+                var storage = new Storage();
+                storage.AddFileToStorage(readFile);
+                returnFiles.Add(storage);
+            }
+
+            return returnFiles;
+        }
+    }
+}
