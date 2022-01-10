1188546c84fa26a848275aed3f96574a3e73c787
viclown <76227700+viclown@users.noreply.github.com>
2022-01-10 22:04:59 +0300

    fixed transation and transfer method DeclineTransaction

--- Banks/Classes/Transaction.cs
+++ Banks/Classes/Transaction.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using Banks.Classes.BankAccount;
+using Banks.Tools;
 
 namespace Banks.Classes
 {
     public class Transaction
     {
-        public Transaction(Client receiver, Client sender, double amountOfTransaction, DateTime transactionTime)
+        public Transaction(Account receiver, Account sender, double amountOfTransaction, DateTime transactionTime)
         {
             Receiver = receiver;
             Sender = sender;
@@ -12,10 +14,21 @@ namespace Banks.Classes
             TransactionTime = transactionTime;
         }
 
-        public Client Receiver { get; }
-        public Client Sender { get; }
+        public Account Receiver { get; }
+        public Account Sender { get; }
         public double AmountOfTransaction { get; }
         public DateTime TransactionTime { get; }
         public bool WasDeclined { get; set; } = false;
+
+        public void DeclineTransaction()
+        {
+            if (WasDeclined)
+            {
+                throw new DeclinedTransactionException($"Transaction of {TransactionTime} from {Sender.Client.Name + Sender.Client.Surname} to {Receiver.Client.Name + Receiver.Client.Surname} was already declined");
+            }
+
+            WasDeclined = true;
+            Sender.Money += AmountOfTransaction;
+        }
     }
 }
