abb354c9a700b765001f8057443edc6288bd1b3d
viclown <76227700+viclown@users.noreply.github.com>
2022-01-10 22:08:11 +0300

    fixed client's setters, renamed build method in client's builder

--- Banks/Classes/Client.cs
+++ Banks/Classes/Client.cs
@@ -15,38 +15,13 @@ namespace Banks.Classes
             Passport = passport;
         }
 
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Address { get; set; }
-        public long Passport { get; set; }
+        public string Name { get; }
+        public string Surname { get; }
+        public string Address { get; }
+        public long Passport { get; }
         public int Id { get; set; }
-        public bool IsSuspicious { get; set; } = true;
-        public bool IsSubscribedToNotifications { get; set; } = false;
-        public List<DebitAccount> DebitAccounts { get; set; } = new List<DebitAccount>();
-        public List<DepositAccount> DepositAccounts { get; set; } = new List<DepositAccount>();
-        public List<CreditAccount> CreditAccounts { get; set; } = new List<CreditAccount>();
-        public List<string> Notifications { get; set; }
-
-        public void SubscribeToNotifications()
-        {
-            IsSubscribedToNotifications = true;
-        }
-
-        public void SetAddress(string address)
-        {
-            Address = address;
-            if (Passport != 0)
-                IsSuspicious = false;
-        }
-
-        public void SetPassport(long passport)
-        {
-            if (passport < Math.Pow(10, 9) || passport >= Math.Pow(10, 10))
-                throw new InvalidPassportException("Passport consists of 10 numbers, please, make sure the entered data is correct");
-            Passport = passport;
-
-            if (Address != null)
-                IsSuspicious = false;
-        }
+        public bool IsSuspicious => Passport == 0 || Address == null;
+        public List<Account> Accounts { get; } = new List<Account>();
+        public List<string> Notifications { get; } = new List<string>();
     }
 }
--- Banks/Classes/ClientBuilder.cs
+++ Banks/Classes/ClientBuilder.cs
@@ -29,7 +29,7 @@ namespace Banks.Classes
             _passport = passport;
         }
 
-        public Client Build()
+        public Client BuildClient()
         {
             return new Client(_name, _surname, _address, _passport);
         }
